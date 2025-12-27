7499e79b99b1239c5f45856112a7f77960bc0144
garlie0206 <garlie0206@gmail.com>
2025-12-27 15:11:43 +0000

    amended api key

--- SRC/index.js
+++ SRC/index.js
@@ -11,7 +11,7 @@ function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
-  let apiKey = "3ob966ee5c00a195346b3f35b10dtd623";
+  let apiKey = "ob966ee5c00a195346b3f35b10dtd623";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
